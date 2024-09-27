71e69da06b534075ac4f1711aef9937e18ef1f0e
Anna Khramova <xaa2128@mail.ru>
2024-09-27 17:25:10 +0300

    Add @Query

--- src/main/java/ru/vtb/vzss/neto104/repository/PersonsRepository.java
+++ src/main/java/ru/vtb/vzss/neto104/repository/PersonsRepository.java
@@ -1,6 +1,7 @@
 package ru.vtb.vzss.neto104.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.vtb.vzss.neto104.model.Persons;
 
@@ -10,10 +11,13 @@ import java.util.Optional;
 @Repository
 public interface PersonsRepository extends JpaRepository<Persons, Integer> {
 
+    @Query("select p from Persons p where p.cityOfLiving = :city")
     List<Persons> findByCityOfLiving(String city);
 
+    @Query("select p from Persons p where p.age < :age order by p.age")
     List<Persons> findByAgeLessThanOrderByAge(int age);
 
+    @Query("select p from Persons p where p.name = :name and p.surname = :surname")
     Optional<Persons> findByNameAndSurname(String name, String surname);
 
 }
